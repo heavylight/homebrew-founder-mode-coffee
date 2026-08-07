245f94edcaa15176ee626df81415e87714736fdd
bossofcoffee <foundermodecoffee@gmail.com>
2026-08-07 16:58:56 -0700

    [verified] chore: update CLI to v2.2.1

--- Formula/founder-mode-coffee.rb
+++ Formula/founder-mode-coffee.rb
@@ -1,16 +1,16 @@
 class FounderModeCoffee < Formula
   desc "Coffee for people who build things"
   homepage "https://foundermodecoffee.com"
-  url "https://github.com/bossofcoffee/founder-mode-coffee-cli/releases/download/v2.2.0/founder-mode-coffee-2.2.0.tar.gz"
-  sha256 "34e4164c6d4550c88256a7514698722738ab54a026e1bad28b529403f5fe8813"
+  url "https://github.com/bossofcoffee/founder-mode-coffee-cli/releases/download/v2.2.1/founder-mode-coffee-2.2.1.tar.gz"
+  sha256 "1539f5c43563a3fb056e7baa7a794dece58ccad5f3b4e873e152f0a1ccdc7486"
   license "MIT"
-  version "2.2.0"
+  version "2.2.1"
 
   def install
     bin.install "founder-mode-coffee"
   end
 
   test do
-    assert_match "founder-mode-coffee 2.2.0", shell_output("#{bin}/founder-mode-coffee --version")
+    assert_match "founder-mode-coffee 2.2.1", shell_output("#{bin}/founder-mode-coffee --version")
   end
 end
